2b5b56126439d39ab0c7cc9bf2b5de8d0a41518a
Ngo <thamtuconan78@gmail.com>
2020-04-18 10:09:10 +0700

    Initial commit

---
+++ app/src/main/java/vn/edu/ntu/ngovantien/ngovantien_57131775_lifecycle/MainActivity.java
@@ -0,0 +1,14 @@
+    package vn.edu.ntu.ngovantien.ngovantien_57131775_lifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
